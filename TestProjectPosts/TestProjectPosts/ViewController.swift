7c57ee71649a494f4d1dce4b617aa333c6992375
ErmakPashaArsenal <pavelermak1991@gmail.com>
2022-07-25 22:39:29 +0300

    create url add to posts json file

--- TestProjectPosts/TestProjectPosts/ViewController.swift
+++ TestProjectPosts/TestProjectPosts/ViewController.swift
@@ -13,6 +13,17 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let urlString = "https://raw.githubusercontent.com/anton-natife/jsons/master/api/main.json"
+        
+        guard let url = URL(string: urlString) else { return }
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                print(error)
+                return
+            }
+        }
+        
     }
 
 
