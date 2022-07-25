682ba2f685de0555e0bf3e8da12805cfc5a5d3b9
ErmakPashaFCArsenal <01pashaermak1991@gmail.com>
2022-07-25 16:59:23 +0300

    first commit

---
+++ TestProjectPosts/TestProjectPosts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestProjectPosts
+//
+//  Created by macbook on 25.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
