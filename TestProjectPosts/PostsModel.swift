5af4391dc06361cc0092072b5f777e659589af07
ErmakPashaArsenal <pavelermak1991@gmail.com>
2022-07-25 21:51:15 +0300

    add to PostsModel

---
+++ TestProjectPosts/PostsModel.swift
@@ -0,0 +1,20 @@
+//
+//  PostsModel.swift
+//  TestProjectPosts
+//
+//  Created by macbook on 25.07.2022.
+//
+
+import Foundation
+
+struct PostsModel: Decodable {
+    var posts:[PostCellData]
+}
+
+struct PostCellData: Decodable {
+    let postId: Int
+    let timeshamp: Int
+    let title: String
+    let preview_text: String
+    let likes_count: Int
+}
