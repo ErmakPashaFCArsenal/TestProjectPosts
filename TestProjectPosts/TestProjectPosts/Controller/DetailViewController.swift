4f02d93317637c317c2a4ff3d7382c579f28a07d
ErmakPashaArsenal <pavelermak1991@gmail.com>
2022-07-27 19:46:56 +0300

    create DetailViewController

---
+++ TestProjectPosts/TestProjectPosts/Controller/DetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DetailViewController.swift
+//  TestProjectPosts
+//
+//  Created by macbook on 27.07.2022.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
