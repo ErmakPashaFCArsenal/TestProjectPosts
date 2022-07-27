0314acf3bc4e140981b9fcb040373dd701e5d3aa
ErmakPashaArsenal <pavelermak1991@gmail.com>
2022-07-27 20:32:32 +0300

    create postModel

--- TestProjectPosts/TestProjectPosts/Controller/DetailViewController.swift
+++ TestProjectPosts/TestProjectPosts/Controller/DetailViewController.swift
@@ -9,6 +9,11 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
+    @IBOutlet weak var postTextDescription: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var postModel:PostModel?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
